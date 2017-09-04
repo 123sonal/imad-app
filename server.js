4f355e1e8308765e96dee84c5d37b2ac52f78742
sonal pandharinath pimpale <123sonalpimple@gmail.com>
2017-09-04 20:19:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,53 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title: 'Article one | sonal pimpale',
+    heading: 'Article One',
+    date:'sep5,2016',
+    Content:
+             `<p>
+                 This is content of my first article.
+             </p>'
+};
+Function createTemplate(data){
+   var title=data.title;
+   var date=data.date;
+   var heading=data.heading;
+   var content=data.content;
+
+   var htmlTemplate= `
+    <html>
+    <head>
+        <meta name="viewport" content="width-device-width, initial-scale=1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+        <title>
+            ${title}
+        </title>
+    </head>
+    <body>
+        <div class="container">
+            <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+         <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>   
+        <div>
+            <p>
+                ${content}
+            </p>
+        </div>
+        </div>
+    </body>
+</html>
+`;
+  return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
