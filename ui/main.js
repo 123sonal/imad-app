22f5e7f19637bcf62d39760f4634f0c57eb3816a
sonal pandharinath pimpale <123sonalpimple@gmail.com>
2017-09-06 20:25:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,21 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function ()
-{
-    counter=counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+{   
+    // create the request object
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function (){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //Take some Action
+            if(request.status ===200){
+                var counter = request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+            //Not Done Yet
+    };
+       // make the request
+       request.open('GET' , 'http://u123sonalpimple.imad.hasura-app.io/counter', true);
+       request.send(null);
 };
